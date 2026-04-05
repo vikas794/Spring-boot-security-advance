d72e1d01bf290271ce18e41497a1032006cf3277
Vikas Jaiswal <vikasjaiswal794@gmail.com>
2026-04-05 16:12:30 +0530

    modified the file and folder structure

--- security-learning-platform/src/main/java/com/example/security/core/config/SecurityConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.example.security.core.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-public interface SecurityConfigurer {
-    void configure(HttpSecurity http) throws Exception;
-}
